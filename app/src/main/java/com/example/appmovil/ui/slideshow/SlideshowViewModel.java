024e676dbd85e17e3d2301f6869b5c84cfd8b2f3
AndreaCTS <90526089+AndreaCTS@users.noreply.github.com>
2024-11-17 19:35:02 -0500

    prueba 1

--- app/src/main/java/com/example/appmovil/ui/slideshow/SlideshowViewModel.java
+++ app/src/main/java/com/example/appmovil/ui/slideshow/SlideshowViewModel.java
@@ -10,7 +10,7 @@ public class SlideshowViewModel extends ViewModel {
 
     public SlideshowViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Soy Laura");
+        mText.setValue("Soy Andrea, hasta la vista baby");
     }
 
     public LiveData<String> getText() {
